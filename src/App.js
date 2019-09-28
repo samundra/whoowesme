69afe1c686b81274e2bc83f232c13dd6b73b72bb
Samundra Shrestha <samundra.shr@gmail.com>
2019-09-29 03:10:28 +0700

    add sample component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Button } from 'antd';
 import './App.css';
+import { MainNavigation } from '../src/Components/navigation/MainNavigation';
 
 function App() {
   return (
     <div className="App">
-      <div style={{ height: '100%', background: '#cccccc' }}>
+      <div
+        style={{
+          textAlign: 'center',
+          display: 'block',
+          width: '100%',
+        }}
+      >
+        <MainNavigation />
+      </div>
+      <div style={{ height: '100%' }}>
         <Button type="primary">Button</Button>
       </div>
     </div>
