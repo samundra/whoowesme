cadaff12249978abe664cdc2b6b744509ae6a31c
pradeepravi-cse <pradeep@capsule8.com>
2019-10-10 20:30:01 +0530

    Updated: Single quote convention

--- Store/Store.tsx
+++ Store/Store.tsx
@@ -1,8 +1,7 @@
-import { createStore } from "redux";
+import { createStore } from 'redux';
 
 export default createStore(
   // your reducer
   //@ts-ignore
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from "react";
-import "./App.css";
-import { Row, Col } from "antd";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import MainNavigation from "./Components/navigation/MainNavigation";
-import SidebarNavigation from "./Components/navigation/SidebarNavigation";
+import React from 'react';
+import './App.css';
+import { Row, Col } from 'antd';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import MainNavigation from './Components/navigation/MainNavigation';
+import SidebarNavigation from './Components/navigation/SidebarNavigation';
 import SendInvitation, {
   menu as sendInvitationMenu
-} from "Pages/SendInvitation";
-import ManageFriend, { menu as manageFriendMenu } from "Pages/ManageFriend";
-import { Provider } from "react-redux";
-import store from "./Store/Store";
+} from 'Pages/SendInvitation';
+import ManageFriend, { menu as manageFriendMenu } from 'Pages/ManageFriend';
+import { Provider } from 'react-redux';
+import store from './Store/Store';
 const contentStyle = {
-  paddingLeft: "10px",
-  textAlign: "left",
-  overflow: "auto",
-  height: "auto",
-  minHeight: "400px"
+  paddingLeft: '10px',
+  textAlign: 'left',
+  overflow: 'auto',
+  height: 'auto',
+  minHeight: '400px'
 } as React.CSSProperties;
 
 const navigationStyle = {
-  width: "1024px",
-  margin: "0 auto"
+  width: '1024px',
+  margin: '0 auto'
 } as React.CSSProperties;
 
 type AppProps = {
@@ -30,12 +30,12 @@ type AppProps = {
 const App: React.FunctionComponent<AppProps> = () => {
   return (
     <Provider store={store}>
-      <Row className="App">
+      <Row className='App'>
         <Router>
           <Row
             style={{
-              textAlign: "center",
-              display: "block"
+              textAlign: 'center',
+              display: 'block'
             }}
           >
             <MainNavigation />
