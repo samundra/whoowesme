d174faf532acf0cab70bd265d7ac3946a2e8968e
pradeepravi-cse <pradeep@capsule8.com>
2019-10-10 17:56:49 +0530

    Revert "Redux Store Base included"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from "react";
-import "./App.css";
-import { Row, Col } from "antd";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import MainNavigation from "./Components/navigation/MainNavigation";
-import SidebarNavigation from "./Components/navigation/SidebarNavigation";
+import React from 'react';
+import './App.css';
+import { Row, Col } from 'antd';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import MainNavigation from './Components/navigation/MainNavigation';
+import SidebarNavigation from './Components/navigation/SidebarNavigation';
 import SendInvitation, {
-  menu as sendInvitationMenu
-} from "Pages/SendInvitation";
-import ManageFriend, { menu as manageFriendMenu } from "Pages/ManageFriend";
-import { Provider } from "react-redux";
-import store from "./Store/Store";
+  menu as sendInvitationMenu,
+} from 'Pages/SendInvitation';
+import ManageFriend, { menu as manageFriendMenu } from 'Pages/ManageFriend';
+
 const contentStyle = {
-  paddingLeft: "10px",
-  textAlign: "left",
-  overflow: "auto",
-  height: "auto",
-  minHeight: "400px"
+  paddingLeft: '10px',
+  textAlign: 'left',
+  overflow: 'auto',
+  height: 'auto',
+  minHeight: '400px',
 } as React.CSSProperties;
 
 const navigationStyle = {
-  width: "1024px",
-  margin: "0 auto"
+  width: '1024px',
+  margin: '0 auto',
 } as React.CSSProperties;
 
 type AppProps = {
@@ -29,37 +28,32 @@ type AppProps = {
 
 const App: React.FunctionComponent<AppProps> = () => {
   return (
-    <Provider store={store}>
-      <Row className="App">
-        <Router>
-          <Row
-            style={{
-              textAlign: "center",
-              display: "block"
-            }}
-          >
-            <MainNavigation />
-            <Row style={navigationStyle}>
-              <SidebarNavigation />
-              <Col style={contentStyle}>
-                <Row>
-                  <Switch>
-                    <Route
-                      path={manageFriendMenu.to}
-                      component={ManageFriend}
-                    />
-                    <Route
-                      path={sendInvitationMenu.to}
-                      component={SendInvitation}
-                    />
-                  </Switch>
-                </Row>
-              </Col>
-            </Row>
+    <Row className="App">
+      <Router>
+        <Row
+          style={{
+            textAlign: 'center',
+            display: 'block',
+          }}
+        >
+          <MainNavigation />
+          <Row style={navigationStyle}>
+            <SidebarNavigation />
+            <Col style={contentStyle}>
+              <Row>
+                <Switch>
+                  <Route path={manageFriendMenu.to} component={ManageFriend} />
+                  <Route
+                    path={sendInvitationMenu.to}
+                    component={SendInvitation}
+                  />
+                </Switch>
+              </Row>
+            </Col>
           </Row>
-        </Router>
-      </Row>
-    </Provider>
+        </Row>
+      </Router>
+    </Row>
   );
 };
 
--- src/Store/actions/ActionType.tsx
+++
@@ -1,1 +0,0 @@
-export const MONEY = "MONEY";
