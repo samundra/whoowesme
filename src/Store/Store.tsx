f50b2c9e519b8b6f86cf1345f872866227391cc4
Samundra Shrestha <samundra.shr@gmail.com>
2019-11-14 09:08:00 +0700

    add store

--- src/Store/Store.tsx
+++ src/Store/Store.tsx
@@ -1,10 +1,17 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+// Fix: Add store
 const rootReducer = combineReducers({});
+const composeEnhancers = composeWithDevTools({});
 
-export default createStore(
-  // your reducer
-  //@ts-ignore
-  rootReducer
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(rootReducer, composeEnhancers());
+
+export default store;
+
+// export default createStore(
+//   // your reducer
+//   //@ts-ignore
+//   rootReducer
+//   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
