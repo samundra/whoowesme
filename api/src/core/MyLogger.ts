8e01011266cde4bec6bc423dd2465a54a28f2b89
Samundra Shrestha <samundra.shr@gmail.com>
2020-10-05 10:03:32 +0700

    use backend instead of api

--- api/src/core/MyLogger.ts
+++
@@ -1,7 +0,0 @@
-import { Logger } from '@nestjs/common';
-
-export class MyLogger extends Logger {
-  error(message: string, trace: string) {
-    super.error(message, trace);
-  }
-}
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
