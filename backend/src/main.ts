45a0c3718234ba4c3186185ff568a72ea59bb74a
Samundra Shrestha <samundra.shr@gmail.com>
2020-10-08 00:38:35 +0700

    Add api.whoowesme.local to docker-compose

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const APPLICATION_PORT = 5000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(APPLICATION_PORT);
 }
+
 bootstrap();
