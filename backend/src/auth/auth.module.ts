eb367f516fb185cf9da522ad88936f68d30af43b
Samundra Shrestha <samundra.shr@gmail.com>
2020-10-13 23:17:05 +0700

    Code cleanup

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import {UsersModule} from "../users/users.module";
-import {PassportModule} from "@nestjs/passport";
-import {LocalStrategy} from "./local.strategy";
+import { UsersModule } from "../users/users.module";
+import { PassportModule } from "@nestjs/passport";
+import { LocalStrategy } from "./local.strategy";
 
 @Module({
   imports: [UsersModule, PassportModule],
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import {UsersService} from "../users/users.service";
+import { UsersService } from "../users/users.service";
 
 @Injectable()
 export class AuthService {
