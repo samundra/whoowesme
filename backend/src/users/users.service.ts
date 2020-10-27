75bf48e18acf6138b1590531e1a048471c60ed84
Samundra Shrestha <samundra.shr@gmail.com>
2020-10-27 21:24:14 +0700

    chore(auth): code cleanup

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -34,25 +34,6 @@ export class UsersService {
     ]
   }
 
-  // async createMany(users: User[]) {
-  //   const queryRunner = this.connection.createQueryRunner()
-  //
-  //   await queryRunner.connect()
-  //   await queryRunner.startTransaction()
-  //   try {
-  //     await queryRunner.manager.save(users[0])
-  //     await queryRunner.manager.save(users[1])
-  //
-  //     await queryRunner.commitTransaction()
-  //   } catch (err) {
-  //     // since we have errors lets rollback the changes we made
-  //     await queryRunner.rollbackTransaction()
-  //   } finally {
-  //     // you need to release a queryRunner which was manually instantiated
-  //     await queryRunner.release()
-  //   }
-  // }
-
   findAll(): Promise<User[]> {
     return this.usersRepository.find()
   }
