d510291e0c3742558ef37e2f9305915cc746e1df
S <desushil@gmail.com>
2020-10-27 22:09:19 +0700

    Add crud for transactions

--- backend/src/transactions/dto/transaction.dto.ts
+++ backend/src/transactions/dto/transaction.dto.ts
@@ -1,6 +1,15 @@
+import { IsOptional, IsIn, IsNotEmpty } from "class-validator";
+
 export class CreateTransactionDto {
+  @IsNotEmpty()
   amount: number;
+
+  @IsOptional()
   description: string;
+
+  @IsOptional()
   date: Date;
+
+  @IsOptional()
   categories: string[];
 }
