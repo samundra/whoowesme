7284b86f5ba9b436b4a85c18b51a86bed8ff5435
Samundra Shrestha <samundra.shr@gmail.com>
2021-10-10 18:29:27 +0700

    chore(env): update configs (#286)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,8 @@ import { ApiLogger } from './logger/logger.service'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
-  app.useLogger(new ApiLogger())
+  const logger = new ApiLogger()
+  app.useLogger(logger)
 
   // see https://expressjs.com/en/guide/behind-proxies.html
   app.set('trust proxy', 1)
@@ -27,11 +28,11 @@ async function bootstrap() {
   )
 
   app.enableCors({
-    origin: [process.env.FRONTEND_ORIGIN, 'https://whooweme.netlify.app', 'http://localhost:5000'],
+    origin: [process.env.FRONTEND_ORIGIN, 'https://whooweme.netlify.app', 'http://localhost:8000'],
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 
-  const config = new DocumentBuilder()
+  const documentConfig = new DocumentBuilder()
     .setTitle('Whoowesme API')
     .setDescription('All API')
     .addServer('http://whoapi.samundra.com.np', 'v1')
@@ -46,15 +47,23 @@ async function bootstrap() {
     customSiteTitle: 'API Specs',
   }
 
-  const document = SwaggerModule.createDocument(app, config)
+  const document = SwaggerModule.createDocument(app, documentConfig)
   SwaggerModule.setup('api', app, document, customOptions)
 
   // Starts listening for shutdown hooks
   // Enable with caution: https://docs.nestjs.com/fundamentals/lifecycle-events#application-shutdown
   // app.enableShutdownHooks()
 
-  const port = +process.env.APP_PORT || 5000
-  await app.listen(port)
+  const port = parseInt(process.env.BACKEND_PORT, 10) || 5000
+  const host = process.env.BACKEND_HOST || 'localhost'
+
+  app
+    .listen(port, host)
+    .then(async () => {
+      const appUrl = await app.getUrl()
+      logger.log(`Application running at ${appUrl}/v1`)
+    })
+    .catch(console.error)
 }
 
 bootstrap()
