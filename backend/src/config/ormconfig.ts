4ec19a71e6f1c8b18234430b6bd4786f09055898
Samundra Shrestha <samundra.shr@gmail.com>
2021-10-10 15:05:34 +0700

    chore(env): update configs

--- backend/src/config/ormconfig.ts
+++ backend/src/config/ormconfig.ts
@@ -25,11 +25,11 @@ if (error) {
 const ormConfig = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: +process.env.POSTGRES_PORT || 5432,
+  port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
-  connectTimeoutMS: +process.env.POSTGRES_DB_TIMEOUT || 10 * 1000,
+  connectTimeoutMS: parseInt(process.env.POSTGRES_DB_TIMEOUT, 10) || 10 * 1000,
   poolErrorHandler: error => {
     console.log('Connection Pool error in database')
     console.error(error)
